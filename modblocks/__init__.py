bb520af4ed63cd4929801f33b8b3aded8e0b479d
Alexander <59180358+Alexiooon@users.noreply.github.com>
2025-02-15 10:30:36 +0100

    Small fixes, add chemical plant

--- modblocks/__init__.py
+++ modblocks/__init__.py
@@ -1,13 +1,15 @@
 """Init module."""
 
-from assembling_machine import AssemblingMachine
-from em_plant import ElectromagneticPlant
-from foundry import Foundry
-from science import PRODUCTIVITY_OPTIONS, GlobalScienceProgress
+from .assembling_machine import AssemblingMachine
+from .chemical_plant import ChemicalPlant
+from .em_plant import ElectromagneticPlant
+from .foundry import Foundry
+from .science import PRODUCTIVITY_OPTIONS, GlobalScienceProgress
 
 __all__ = [
     "PRODUCTIVITY_OPTIONS",
     "AssemblingMachine",
+    "ChemicalPlant",
     "ElectromagneticPlant",
     "Foundry",
     "GlobalScienceProgress"
--- modblocks/chemical_plant.py
+++ modblocks/chemical_plant.py
@@ -1,16 +1,16 @@
-"""Module for assembling machines."""
+"""Module for chemical plants."""
 from .crafting_station import BaseCraftingStation
 
-SPEED_BY_QUALITY = (1.25, 1.625, 2.0, 2.375, 3.125)
+SPEED_BY_QUALITY = (1.0, 1.3, 1.6, 1.9, 2.5)
 
 
-class AssemblingMachine(BaseCraftingStation):
-    """Assembling Machine 3 class."""
+class ChemicalPlant(BaseCraftingStation):
+    """Chemical plant class."""
 
     def __init__(self, quality: int):
-        """Initialize a new Assembling Machine 3."""
+        """Initialize a new Chemical plant."""
         super().__init__()
         self._base_speed: float = SPEED_BY_QUALITY[quality]
         self._base_prod: float = 1.0
-        self._module_slots: int = 4
+        self._module_slots: int = 3
         self._modules = [None] * self._module_slots
