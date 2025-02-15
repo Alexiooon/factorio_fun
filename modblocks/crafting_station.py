27aaeb501fce61b6f2f5665cd5e2d6918d3d4843
Alexander <59180358+Alexiooon@users.noreply.github.com>
2025-02-15 10:30:20 +0100

    Add support for Recipe in crafting station

--- modblocks/crafting_station.py
+++ modblocks/crafting_station.py
@@ -1,4 +1,11 @@
 """Base object for generic crafting station."""
+import json
+import os
+
+from .recipe import Recipe
+
+with open(os.path.join("data", "recipes.json"), "r", encoding="utf8") as _file:
+    RECIPES = json.load(_file)
 
 
 class BaseCraftingStation():
@@ -6,10 +13,16 @@ class BaseCraftingStation():
 
     def __init__(self):
         """Initialize a new generic crafting station."""
+        # Crafting station parameters
         self._base_speed: float = 1.0
         self._base_prod: float = 1.0
+
+        # Modules
         self._module_slots: int = 0
         self._modules = [None] * self._module_slots
+
+        # Crafting recipe
+        self.recipe: Recipe | None = None
 
     @property
     def modules(self):
@@ -48,5 +61,17 @@ class BaseCraftingStation():
         given base productivity, modules etc.
         """
         # TODO: Include science bonus (if applicable)
-        prod_modules = sum(module["productivity"] for module in self._modules)
-        return self._base_prod * prod_modules
+        prod_modules = sum(module["productivity"] for module in self._modules if module is not None)
+        return self._base_prod + (1 * prod_modules)
+
+    @property
+    def input(self) -> dict:
+        """Get the input items and their quantities per second."""
+        base_input = (self.crafting_speed / self.recipe.time)
+        return {name: base_input * quantity for name, quantity in self.recipe.input.items()}
+
+    @property
+    def output(self) -> dict:
+        """Get the output items and their quantities per second."""
+        base_output = (self.crafting_speed / self.recipe.time) * self.productivity
+        return {name: base_output * quantity for name, quantity in self.recipe.output.items()}
