9c8ab02809d0522330b906cb4d39ed1c864959e1
Alexander <59180358+Alexiooon@users.noreply.github.com>
2025-08-01 17:10:35 +0200

    Further visualization using graphviz

--- graph/visualize.py
+++ graph/visualize.py
@@ -1,22 +1,71 @@
 """Scripts for visualizing CraftingGraphs."""
 
+import logging
+import math
 import os
 
 import graphviz
+
+from game.buildings import BaseCraftingStation
 
 from .crafting_graph import CraftingGraph
 
 OUTPUT_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "_out")
 
+# Default attributes for graphviz
+GRAPH_ATTRIBUTES = {}
+NODE_ATTRIBUTES = {
+    "fillcolor": "lightgray",
+    "shape": "rect",
+    "style": "filled",
+}
+EDGE_ATTRIBUTES = {}
 
-def visualize(graph: CraftingGraph, filename="mygraph"):
+_logger = logging.getLogger(__name__)
+
+
+# TODO (Alex): Use HTML for nicer formatting
+def _get_node_label(node: BaseCraftingStation, node_data: dict) -> str:
+    """Generate a node label."""
+    # Title/header
+    stations = node_data.get("stations")
+    if stations is None:
+        _logger.warning("Node %s has no 'stations' data", node)
+        stations = 1
+    header = str(math.ceil(stations)) + "x " + str(node)
+
+    # Inputs
+    input_items = "Input:"
+    for item, quantity in node.input.items():
+        input_items += rf"\l- {item}: {quantity * stations:.1f}/s"
+
+    # Outputs
+    output_items = "Output:"
+    for item, quantity in node.output.items():
+        output_items += rf"\l- {item}: {quantity * stations:.1f}/s"
+
+    return r"\l".join([header, input_items, output_items, ""])  # An extra "\l" is needed at the end
+
+
+def visualize(graph: CraftingGraph, filename: str = "graph"):
     """Convert graph to Graphviz for visualization."""
-    dot = graphviz.Digraph(engine="dot")
+    dot = graphviz.Digraph(
+        graph_attr=GRAPH_ATTRIBUTES,
+        node_attr=NODE_ATTRIBUTES,
+        edge_attr=EDGE_ATTRIBUTES,
+        engine="dot",
+    )
 
-    for node in graph.nodes():
-        dot.node(name=str(id(node)), label=str(node), style="filled", shape="rect")
+    # Nodes
+    for node, data in graph.nodes.items():
+        label = _get_node_label(node, data)
+        dot.node(name=str(id(node)), label=label, labeljust="l")
 
+    # Edges
     for a, b in graph.edges():
-        dot.edge(str(id(a)), str(id(b)))
+        dot.edge(tail_name=str(id(a)), head_name=str(id(b)))
 
-    dot.render(filename=os.path.join(OUTPUT_PATH, filename), view="dot")
+    # Render and save
+    filename = os.path.join(OUTPUT_PATH, filename)
+    dot.render(filename=filename, view="dot")
+    _logger.info("Graph saved to %s", filename)
